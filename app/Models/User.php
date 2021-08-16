2acb3c2f994e6a12a51d125842713c53b9d26d0b
FatoomMFayad <fatoomfayyad@gmail.com>
2021-08-16 13:08:48 +0300

    using uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,15 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    public $incrementing = false;
+
+
 
     /**
      * The attributes that are mass assignable.
@@ -40,4 +45,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+
+        static::creating(function ($model) {
+            //generete a unique id for the model
+            $model->id = Str::uuid();
+        });
+    }
 }
